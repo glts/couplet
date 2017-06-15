66f1ee03a577e7740c866baadc8b5eb7057a67fb
David Bürgin <dbuergin@gluet.ch>
2017-06-15 21:37:23 +0200

    Drop org.clojure/core.specs.alpha dependency
    This is included by Clojure itself, so no need to depend on it directly.

--- project.clj
+++ project.clj
@@ -4,8 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
-                 [org.clojure/spec.alpha "0.1.123"]
-                 [org.clojure/core.specs.alpha "0.1.10"]]
+                 [org.clojure/spec.alpha "0.1.123"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]
                                   [criterium "0.4.4"]]}}
   :plugins [[lein-codox "0.10.3"]])
