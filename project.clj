c5080ecca628c9044103202ed380fd7d19e6327f
David Bürgin <dbuergin@gluet.ch>
2017-06-15 19:03:02 +0200

    Restrict test dependencies to dev profile

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
                  [org.clojure/spec.alpha "0.1.123"]
-                 [org.clojure/core.specs.alpha "0.1.10"]
-                 [org.clojure/test.check "0.9.0"]
-                 [criterium "0.4.4"]]
+                 [org.clojure/core.specs.alpha "0.1.10"]]
+  :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]
+                                  [criterium "0.4.4"]]}}
   :plugins [[lein-codox "0.10.3"]])
