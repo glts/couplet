0be79ce75509c4ec325537dce475ac5afff11492
David Bürgin <dbuergin@gluet.ch>
2017-06-25 15:42:50 +0200

    Bump version to 0.0.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ch.gluet/couplet "0.0.1"
+(defproject ch.gluet/couplet "0.0.2"
   :description "Unicode code points support for Clojure"
   :url "https://github.com/glts/couplet"
   :license {:name "Eclipse Public License"
