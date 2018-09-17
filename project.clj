8cb788c42f04a75c73d96c970dfdc861a723cd37
David Bürgin <dbuergin@gluet.ch>
2018-09-17 17:50:37 +0200

    Bump dependencies

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
-                 [org.clojure/spec.alpha "0.1.143" :scope "provided"]]
-  :plugins [[lein-codox "0.10.3"]
-            [lein-jmh "0.2.3"]]
+                 [org.clojure/spec.alpha "0.2.176" :scope "provided"]]
+  :plugins [[lein-codox "0.10.4"]
+            [lein-jmh "0.2.6"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]
                    :source-paths ["dev"]
                    :global-vars {*warn-on-reflection* true}}
